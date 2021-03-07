4c45c767983ca317c2fbf65c4b0e8a0c55808108
Daniel <33216850+dPaliura@users.noreply.github.com>
2021-03-07 10:31:55 +0200

    Bug fix, comments added
    This script provides all mechanisms for inputting different values through terminal: numbers and function. Methods let to input R language expression like 'sqrt(3)' to be recognized as numbers.

--- lab1/input.R
+++ lab1/input.R
@@ -1,6 +1,15 @@
 # Variant number
 .n <- 15
 
+# List of values given as default - according to author's variant for lab work
+# s0  - initial speed of stream
+# v   - speed of boat
+# l   - distance to target point
+# phi - angle of direction from boat position to target
+#       relative to stream vector
+# f   - function of dependence stream speed multiplier of vertical coordinate y.
+#       It defines stream speed via expression:
+#           s(y) = s0 * f(y)
 .default.input <- list(
     s0 = sqrt(.n),
     v = sqrt(.n),
@@ -10,11 +19,15 @@
 )
 
 
-# Do use this method to get function to read single line from terminal.
+
+# Do use this method to get proper function to read single line from terminal.
 # It is needed when it is unknown whether run in interactive mode or interpreted
 ..get.readline.method <- function(){
     ifelse(interactive(),
-           function(prompt='') return(readline(prompt)),
+           function(prompt='') {
+               cat(prompt)
+               return(readline())
+               },
            function(prompt='') {
                cat(prompt)
                return(readLines(con='stdin', n=1))
@@ -23,18 +36,24 @@
 }
 
 
-.read.num.expression <- function(prompt = '',
-                                finite.value=TRUE){
+# Provides mechanism to read from terminal an numeric expression.
+# It does read from terminal while written expression not evaluated as numeric.
+#   prompt       - text written each time before inputting text in terminal
+#   finite.value - logical. Determines whether got expression must be evaluated
+#                  as finite number. If FALSE, values NaN, NA and Inf allowed.
+# Value - expression object evaluating as number
+.read.num.expression <- function(prompt = '', finite.value=TRUE){
     read.line <- ..get.readline.method()
     while(TRUE){
         tryCatch(
             {
                 expr <- parse(text = read.line(prompt))
                 ev <- eval(expr)
-                if (finite.value) {if (is.finite(ev) &
-                                       !is.complex(ev)) return(expr)}
-                else {if (is.numeric(ev)) return(expr)}
-
+                if (length(ev)==1){
+                    if (finite.value) {if (is.finite(ev) &
+                                           !is.complex(ev)) return(expr)}
+                    else {if (is.numeric(ev)) return(expr)}
+                }
             },
             error = function(e){}
         )
@@ -42,6 +61,16 @@
 }
 
 
+# Provides mechanism to read from terminal the numerical function giving it's
+# body expression. For example, on call with no arguments, if write in terminal
+# 'sqrt(x+log(x+1))' it will return object:
+#                               function(x) sqrt(x+log(x+1))
+#   prompt      - text prompted to user before writing in terminal.
+#                 It repeats on incorrect inputs.
+#   arguments   - character vector of arguments' names for obtained function.
+#   test.values - numeric vector with values of argument which substitute to
+#                 test obtained function whether it's result numeric.
+# Value - function which expected to return numbers.
 .read.num.function <- function(prompt = 'Write function f(x) body\n',
                               arguments = c('x'),
                               test.values = 1){
@@ -75,7 +104,12 @@
 }
 
 
-.get.model.input <- function(){
+# Method for getting input for task of laboratory work
+# including choose between use values according to variant (named as defaults)
+# and hand input. In second case it is available to use R language expressions
+# in terminal input for each numeric value.
+# Example: default phi value would be written as '15*pi/25' in terminal.
+get.model.input <- function(){
     read.line <- ..get.readline.method()
 
     answer <- read.line("Use default input? y/n/x: ")
@@ -99,6 +133,8 @@
 }
 
 
+# Provides question after working off model to restart program.
+# Value - TRUE if user want to restart and FALSE otherwise.
 ask.for.restart <- function(){
     read.answer <- ..get.readline.method()
 
@@ -110,5 +146,3 @@ ask.for.restart <- function(){
         if (answer == 'n') return(FALSE)
     }
 }
-
-
