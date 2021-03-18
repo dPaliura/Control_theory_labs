6f50d711fefba72d4226edbe22e6f2cad415823d
Daniel <33216850+dPaliura@users.noreply.github.com>
2021-03-18 16:19:52 +0200

    All code moved into main function body. Added close of X11 device for terminal

--- lab1/main.R
+++ lab1/main.R
@@ -1,80 +1,80 @@
-# Function to get path of THIS script file on it's run
-#
-# got from:
-# https://stackoverflow.com/a/36777602
-#
-# improved to return way of script run
-ez.csf <- function() {
-    # http://stackoverflow.com/a/32016824/2292993
-    cmdArgs = commandArgs(trailingOnly = FALSE)
-    needle = "--file="
-    match = grep(needle, cmdArgs)
+# Program main loop
+# Call it to run program
+main <- function(){
+    # Function to get path of THIS script file on it's run
+    #
+    # got from:
+    # https://stackoverflow.com/a/36777602
+    #
+    # improved to return way of script run
+    ez.csf <- function() {
+        # http://stackoverflow.com/a/32016824/2292993
+        cmdArgs = commandArgs(trailingOnly = FALSE)
+        needle = "--file="
+        match = grep(needle, cmdArgs)
 
-    ret <- list(
-        path='',
-        method=''
-    )
+        ret <- list(
+            path='',
+            method=''
+        )
 
-    if (length(match) > 0) {
-        # Rscript via command line
-        ret$path <- normalizePath(sub(needle, "", cmdArgs[match]))
-        ret$method <- 'terminal'
-        return(ret)
-    } else {
-        ls_vars = ls(sys.frames()[[1]])
-        if ("fileName" %in% ls_vars) {
-            # Source'd via RStudio
-            ret$path <- normalizePath(sys.frames()[[1]]$fileName)
-            ret$method <- 'RStudio-source'
+        if (length(match) > 0) {
+            # Rscript via command line
+            ret$path <- normalizePath(sub(needle, "", cmdArgs[match]))
+            ret$method <- 'terminal'
             return(ret)
         } else {
-            if (!is.null(sys.frames()[[1]]$ofile)) {
-                # Source'd via R console
-                ret$path <- normalizePath(sys.frames()[[1]]$ofile)
-                ret$method <- 'R-source'
+            ls_vars = ls(sys.frames()[[1]])
+            if ("fileName" %in% ls_vars) {
+                # Source'd via RStudio
+                ret$path <- normalizePath(sys.frames()[[1]]$fileName)
+                ret$method <- 'RStudio-source'
                 return(ret)
             } else {
-                # RStudio Run Selection
-                # http://stackoverflow.com/a/35842176/2292993
-                require('rstudioapi')
-
-                pth = rstudioapi::getActiveDocumentContext()$path
-                if (pth!='') {
-                    ret$path <- normalizePath(pth)
-                    ret$method <- 'RStudio-run'
+                if (!is.null(sys.frames()[[1]]$ofile)) {
+                    # Source'd via R console
+                    ret$path <- normalizePath(sys.frames()[[1]]$ofile)
+                    ret$method <- 'R-source'
                     return(ret)
                 } else {
-                    # RStudio Console
-                    tryCatch({
-                        pth = rstudioapi::getSourceEditorContext()$path
-                        pth = normalizePath(pth)
-                    }, error = function(e) {
-                        # normalizePath('') issues warning/error
-                        pth = ''
+                    # RStudio Run Selection
+                    # http://stackoverflow.com/a/35842176/2292993
+                    require('rstudioapi')
+
+                    pth = rstudioapi::getActiveDocumentContext()$path
+                    if (pth!='') {
+                        ret$path <- normalizePath(pth)
+                        ret$method <- 'RStudio-run'
+                        return(ret)
+                    } else {
+                        # RStudio Console
+                        tryCatch({
+                            pth = rstudioapi::getSourceEditorContext()$path
+                            pth = normalizePath(pth)
+                        }, error = function(e) {
+                            # normalizePath('') issues warning/error
+                            pth = ''
+                        }
+                        )
+                        ret$path <- pth
+                        ret$method <- 'RStudio-console'
+                        return(ret)
                     }
-                    )
-                    ret$path <- pth
-                    ret$method <- 'RStudio-console'
-                    return(ret)
                 }
             }
         }
     }
-}
 
 
-run <- ez.csf()
-path <- sub('main.R', '', run$path)
+    run <- ez.csf()
+    path <- sub('main.R', '', run$path)
 
+    run.X11 = ifelse(run$method=='terminal',T,F)
 
-source(paste0(path, 'input.R'), echo = FALSE)
-source(paste0(path, 'model.R'), echo = FALSE)
-source(paste0(path, 'output.R'), echo = FALSE)
+    source(paste0(path, 'input.R'), echo = FALSE)
+    source(paste0(path, 'model.R'), echo = FALSE)
+    source(paste0(path, 'output.R'), echo = FALSE)
 
-
-# Program main loop
-# Call it to run program
-main <- function(){
     while (TRUE){
         tryCatch(
             {
@@ -84,7 +84,7 @@ main <- function(){
                 show.waiting()
 
                 model <- build.model(model.input)
-                display.model(model, run.X11=ifelse(run$method=='terminal',T,F))
+                display.model(model, run.X11)
             },
             condition = function(con){
                 print(con)
@@ -92,6 +92,7 @@ main <- function(){
         )
         need.restart <- ask.for.restart()
         if (!need.restart) break()
+        else if (run.X11) dev.off()
     }
 }
 
