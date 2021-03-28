b64813080d91c17b8f77b4f1295bfc3cb329ef9b
Daniel <33216850+dPaliura@users.noreply.github.com>
2021-03-28 13:05:27 +0300

    model finished

--- lab3/model.R
+++ lab3/model.R
@@ -6,24 +6,26 @@
 # N - Number of intervals for splitting the time interval [t1, t2].
 # lambda - regularization parameter. If not NULL, then problem solved
 #          as regularized with value lambda >= 0
-var.prb <- function(func, t1, t2, x1, x2, N=50, lambda = NULL, ...){
-    dt <- (t2-t1)/N
-    times <- (0:N)*dt + t1
-    dx <- (x2-x1)/N
-    X0 <- (1:(N-1))*dx + x1
+var.prb <- function(func, t0, t1, x0, x1, N=50, lambda = NULL,
+                    ...,
+                    method = 'BFGS'){
+    dt <- (t1-t0)/N
+    times <- (0:N)*dt + t0
+    dx <- (x1-x0)/N
+    X0 <- (1:(N-1))*dx + x0
 
     J <- if(is.null(lambda)) function(X){
-        X <- c(x1, X, x2)
+        X <- c(x0, X, x1)
         dX <- (X[(1:N)+1] - X[1:N])/dt
         I <- sapply(1:N, function(i) func(times[i], X[i], dX[i]))
         return(sum(I)*dt)
     } else function(X){
-        X <- c(x1, X, x2)
+        X <- c(x0, X, x1)
         dX <- (X[(1:N)+1] - X[1:N])/dt
         I <- sapply(1:N, function(i) func(times[i], X[i], dX[i]))
         return((sum(I) + lambda*sum(dX^2))*dt)
     }
 
-    opt <- optim(X0, J)
+    opt <- optim(X0, J, method=method, ...)
     return(opt)
 }
