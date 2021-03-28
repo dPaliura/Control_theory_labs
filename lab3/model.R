3502c9c4c86573a416c46cf0df4d878f251e8edd
Daniel <33216850+dPaliura@users.noreply.github.com>
2021-03-28 18:18:13 +0300

    solution wrapped into class
    Methods 'plot' and 'summary' binded to solution class.

--- lab3/model.R
+++ lab3/model.R
@@ -1,12 +1,12 @@
 # Solves variation problem where
-# func - function(t, x, dx) - underintegral function:
+# I = function(t, x, dx) - underintegral function:
 #        time t, value x (x=x(t)), and derivative dx (dx = dx/dt (t))
 # t1, t2 - limits for integral
 # x1, x2 - values for x at moments t1 and t2 (x1 = x(t1), x2 = x(t2))
 # N - Number of intervals for splitting the time interval [t1, t2].
 # lambda - regularization parameter. If not NULL, then problem solved
 #          as regularized with value lambda >= 0
-var.prb <- function(func, t0, t1, x0, x1, N=50, lambda = NULL,
+var.prb <- function(I, t0, t1, x0, x1, N=50, lambda = NULL,
                     ...,
                     method = 'BFGS'){
     dt <- (t1-t0)/N
@@ -17,15 +17,56 @@ var.prb <- function(func, t0, t1, x0, x1, N=50, lambda = NULL,
     J <- if(is.null(lambda)) function(X){
         X <- c(x0, X, x1)
         dX <- (X[(1:N)+1] - X[1:N])/dt
-        I <- sapply(1:N, function(i) func(times[i], X[i], dX[i]))
+        I <- sapply(1:N, function(i) I(times[i], X[i], dX[i]))
         return(sum(I)*dt)
     } else function(X){
         X <- c(x0, X, x1)
         dX <- (X[(1:N)+1] - X[1:N])/dt
-        I <- sapply(1:N, function(i) func(times[i], X[i], dX[i]))
+        I <- sapply(1:N, function(i) I(times[i], X[i], dX[i]))
         return((sum(I) + lambda*sum(dX^2))*dt)
     }
 
     opt <- optim(X0, J, method=method, ...)
-    return(opt)
+    res <- list(
+        I = I, J = J,
+        t0 = t0, t1 = t1,
+        x0 = x0, x1 = x1,
+        N = 50,
+        lambda = lambda,
+        method = method,
+        times = times, X = opt$par,
+        value = opt$value,
+        counts = opt$counts
+    )
+    class(res) <- 'VariationProblemSolution'
+    return(res)
+}
+
+
+plot.VariationProblemSolution <- function(obj, ...){
+    plot(obj$times, c(obj$x0, obj$X, obj$x1),
+         type = 'l',
+         xlab = 'time', ylab = 'x',
+         ...)
+}
+
+
+summary.VariationProblemSolution <- function(obj){
+    nl <- '\n'
+    cat('\tVariatiion Problem solution', nl,
+        'Integrate I(t, x, dx) dt from ', obj$t0, ' to ', obj$t1, nl,
+        'Where I(t, x, dx) = ', deparse(body(obj$I)), nl,
+        'x = x(t): x(',obj$t0,') = ',obj$x0, ', x(',obj$t1,') = ',obj$x1, nl,
+        'Size of interval splitting N = ', obj$N, nl,
+        'J(...) depends on ', obj$N-1, ' variables (inner points)', nl,
+        ifelse(test = !is.null(obj$lambda),
+               yes = paste('Regularized with lambda =', obj$lambda),
+               no ='Regularization wasn\'t used'), nl,
+        'Optimized via optim(par, fn, ...) with method=', obj$method, nl,
+        'Got J optimal value ', obj$value, nl,
+        'While optimiation called J ', obj$counts[1], ' times ',
+        'and its gradient ', ifelse(is.na(obj$counts[2]),
+                                'wasn\'t counted',
+                                paste(obj$counts[2],'times')), '.', nl,
+        sep='')
 }
